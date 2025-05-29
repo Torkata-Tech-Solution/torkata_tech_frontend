09c3cd03c58eca28475d9aa209fc02569364f26c
Fajri Rinaldi Chan <fajri@gariskode.com>
2025-05-29 18:30:37 +0700

    feat: pm2 deploy ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,15 @@
+// ecosystem.config.js
+module.exports = {
+  apps : [{
+    name   : "Torakatatech_sveltekit", // Nama aplikasi Anda
+    script : "build/index.js", // Jalur ke entry point server SvelteKit Anda
+    // Jika Anda menggunakan adapter Node, biasanya ini adalah `build/index.js`
+    // Pastikan jalur ini benar sesuai dengan output build Anda.
+    // Contoh lain: "prerendered/index.js" atau "output/server/index.js"
+    watch  : false, // Set ke true jika Anda ingin PM2 me-restart otomatis saat ada perubahan file
+    env: {
+      NODE_ENV: "production",
+      PORT: 3000
+    }
+  }]
+};
